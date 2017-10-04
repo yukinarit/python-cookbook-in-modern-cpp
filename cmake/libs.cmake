9767951ba4aceaef47eee6eb172cd4d7c52bfcd8
Yukinari Tani <yukinarit84@gmail.com>
2017-10-04 15:34:36 +0900

    Add cpp for calculating_with_dictionaries

---
+++ cmake/libs.cmake
@@ -0,0 +1,8 @@
+set(ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/..")
+
+set(LIBS_DIR "${ROOT_DIR}/libs")
+
+set(RANGE_DIR "${LIBS_DIR}/range-v3")
+set(RANGE_INCLUDE_DIR "${RANGE_DIR}/include")
+
+message("RANGE_INCLUDE_DIR=${RANGE_INCLUDE_DIR}")
---
+++ src/1/calculating_with_dictionaries/example.cpp
@@ -0,0 +1,40 @@
+/// example.cpp
+///
+/// Example of calculating with dictionaries
+
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+
+#include <fmt/format.h>
+#include <range/v3/all.hpp>
+
+using namespace ranges;
+
+int main() {
+    std::unordered_map<std::string, double> prices = {
+        {"ACME", 45.23}, {"AAPL", 612.78}, {"IBM", 205.55}, {"HPQ", 37.20}, {"FB", 10.75},
+    };
+
+    // Find min and max price
+    auto min_it = std::min_element(prices.begin(), prices.end(), [](const auto& a, const auto& b) {
+        return a.second < b.second;
+    });
+    fmt::print("min price: ({}, {})\n", (*min_it).second, (*min_it).first);
+    auto max_it = std::max_element(prices.begin(), prices.end(), [](const auto& a, const auto& b) {
+        return a.second < b.second;
+    });
+    fmt::print("max price: ({}, {})\n", (*max_it).second, (*max_it).first);
+
+    fmt::print("sorted prices:\n");
+    std::vector<std::tuple<std::string, double>> rng =
+        prices | view::transform([](const auto& v) { return std::tuple(v.first, v.second); });
+    auto sorted = std::move(rng) | action::sort([](const auto& a, const auto& b) {
+                      return std::get<1>(a) < std::get<1>(b);
+                  });
+    for (const auto & [ name, price ] : sorted) {
+        fmt::print("     {} {}\n", name, price);
+    }
+
+    return 0;
+}
