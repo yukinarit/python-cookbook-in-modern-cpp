914f6bb087556aa6d0e645b556bb66befa633eb3
Yukinari Tani <yukinarit84@gmail.com>
2017-10-26 23:10:16 +0900

    QIP Add cpp for how_to_flatten_a_nested_sequence

---
+++ src/4/how_to_flatten_a_nested_sequence/example.cpp
@@ -0,0 +1,45 @@
+/// example.cpp
+///
+/// Remove duplicate entries from a sequence while keeping order
+
+#include <iostream>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+#include <range/v3/all.hpp>
+
+using namespace ranges;
+
+template <typename Test, template <typename...> class Ref>
+struct is_specialization : std::false_type {};
+
+template <template <typename...> class Ref, typename... Args>
+struct is_specialization<Ref<Args...>, Ref> : std::true_type {};
+
+template <typename T>
+decltype(auto) flatten(T& items) {
+    auto rng = items | view::for_each([](auto& sub_items) {
+                   using value_type = decltype(sub_items);
+                   if constexpr (is_specialization<value_type, std::vector>::value) {
+                       return flatten(sub_items);
+
+                   } else {
+                       return view::all(sub_items);
+                   }
+               });
+    return rng;
+}
+
+int main(){std::vector<std::vector<std::vector<int>>> items = {{{1}}, {{2}}, {3, 4, {5, 6}, 7}},
+           {{8}},
+           {{9}}};
+auto rng = flatten(items);
+for (auto n : rng) {
+    fmt::print("{}\n", n);
+}
+
+return 0;
+}
