46e8710fb05c3ffddae55aff118aecea18e31275
Yukinari Tani <yukinarit84@gmail.com>
2017-10-27 09:48:20 +0900

    Add cpp for how_to_flatten_a_nested_sequence

--- src/4/how_to_flatten_a_nested_sequence/example.cpp
+++ src/4/how_to_flatten_a_nested_sequence/example.cpp
@@ -2,6 +2,7 @@
 ///
 /// Remove duplicate entries from a sequence while keeping order
 
+#include <any>
 #include <iostream>
 #include <string>
 #include <type_traits>
@@ -20,26 +21,28 @@ template <template <typename...> class Ref, typename... Args>
 struct is_specialization<Ref<Args...>, Ref> : std::true_type {};
 
 template <typename T>
-decltype(auto) flatten(T& items) {
-    auto rng = items | view::for_each([](auto& sub_items) {
-                   using value_type = decltype(sub_items);
-                   if constexpr (is_specialization<value_type, std::vector>::value) {
-                       return flatten(sub_items);
-
-                   } else {
+constexpr decltype(auto) flatten(const std::vector<T>& items) {
+    auto rng = items | view::for_each([](const auto& sub_items) {
+                   using value_type = typename std::decay_t<decltype(sub_items)>::value_type;
+                   if
+                       constexpr(is_specialization<value_type, std::vector>::value) {
+                           return flatten(sub_items);
+                       }
+                   else {
                        return view::all(sub_items);
                    }
                });
     return rng;
 }
 
-int main(){std::vector<std::vector<std::vector<int>>> items = {{{1}}, {{2}}, {3, 4, {5, 6}, 7}},
-           {{8}},
-           {{9}}};
-auto rng = flatten(items);
-for (auto n : rng) {
-    fmt::print("{}\n", n);
-}
+int main() {
+    std::vector<std::vector<std::vector<int>>> items = {
+        {{1}}, {{2}}, {{3}, {4}, {5, 6}, {7}}, {{8}}, {{9}}};
 
-return 0;
+    auto rng = flatten(items);
+    for (auto& n : rng) {
+        fmt::print("{}\n", n);
+    }
+
+    return 0;
 }
