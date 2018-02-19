aa35701b8bf84f8c78f773bdfe31be7f49d512a0
Yukinari Tani <yukinarit84@gmail.com>
2018-02-19 09:28:19 +0900

    Add libzmq and zmqpp

--- cmake/all.cmake
+++ cmake/all.cmake
@@ -6,5 +6,6 @@ set(all_included true)
 include(vars)
 include(fmt)
 include(date)
+include(zmq)
 
 #add_subdirectory(${LIBS_DIR}/boost-cmake ${CMAKE_CURRENT_BINARY_DIR}/boost-cmake)
---
+++ cmake/zmq.cmake
@@ -0,0 +1,20 @@
+if(zmq_included)
+  return()
+endif()
+set(zmq_included true)
+
+include(vars)
+
+set(ZEROMQ_LIBRARY_STATIC ON)
+set(ZMQ_BUILD_TESTS OFF)
+set(WITH_DOC OFF)
+add_subdirectory(${LIBS_DIR}/libzmq ${CMAKE_CURRENT_BINARY_DIR}/libzmq)
+add_subdirectory(${LIBS_DIR}/zmqpp ${CMAKE_CURRENT_BINARY_DIR}/zmqpp)
+
+set(ZMQ_DIR "${LIBS_DIR}/zmqpp")
+set(ZMQ_INCLUDE_DIRS "${ZMQ_DIR}/src")
+set(ZMQ_LIBRARIES "${CMAKE_BINARY_DIR}/zmqpp/libzmq-static.a")
+
+message("ZMQ_DIR=${ZMQ_DIR}")
+message("ZMQ_INCLUDE_DIRS=${ZMQ_INCLUDE_DIRS}")
+message("ZMQ_LIBRARIES=${ZMQ_LIBRARIES}")
